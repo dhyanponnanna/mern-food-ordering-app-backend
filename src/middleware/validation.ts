7701a2c1f00fbb36cb6cdc1c9bbe226781003fa1
Dhyan Ponnanna <153530911+dhyanponnanna@users.noreply.github.com>
2024-05-12 13:51:41 +0530

    Add files via upload

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -20,3 +20,28 @@ export const validateMyUserRequest = [
     body("country").isString().notEmpty().withMessage("Country must be a string"),
     handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+    body("restaurantName").notEmpty().withMessage("Restaurant Name is required"),
+    body("city").notEmpty().withMessage("City is required"),
+    body("country").notEmpty().withMessage("Country is required"),
+    body("deliveryPrice")
+        .isFloat({min: 0})
+        .withMessage("Delivery Price must be as positive number"),
+    body("estimatedDeliveryTime")
+        .isInt({min: 0})
+        .withMessage("Estimated Delivery Time must be as positive integer"),
+    body("cuisines")
+        .isArray()
+        .withMessage("Cuisines must be an array")
+        .not()
+        .isEmpty()
+        .withMessage("Cuisines array cannot be empty"),
+    body("menuItems").isArray().withMessage("Menu Items must be an array"),
+    body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+    body("menuItems.*.price")
+        .isFloat({min: 0})
+        .withMessage("Menu item price is required and must be a positive number"),
+    handleValidationErrors,
+
+]
