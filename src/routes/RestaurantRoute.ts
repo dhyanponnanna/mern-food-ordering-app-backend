82369be726f853e4ae839aeca8b70068ba6d2c95
Dhyan Ponnanna <153530911+dhyanponnanna@users.noreply.github.com>
2024-05-13 18:01:36 +0530

    Add files via upload

---
+++ src/routes/RestaurantRoute.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
+
+const router = express.Router();
+
+router.get("/search/:city",
+ param ("city")
+.isString()
+.trim()
+.notEmpty()
+.withMessage("City parameter must be a valid string"),
+RestaurantController.searchRestaurant
+);
+
+export default router;
